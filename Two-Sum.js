fcbd6933f83daf82db17f3f51ff35d8c972b3ce4
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-05-04 20:08:26 +0300

    Two Sum Problem

---
+++ Two-Sum.js
@@ -0,0 +1,34 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have  exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+function solve(numbers, target) {
+    const previousValues = {};
+
+    for (let index = 0; index < numbers.length; index++) {
+        const currentNumber = numbers[index];
+        const neededValue = target - currentNumber;
+        const index2 = previousValues[neededValue];
+        if (index2 != null) {
+            console.log([index2, index]);
+            return [index2, index];
+        } else {
+            previousValues[currentNumber] = index;
+        }
+    }
+}
+
+solve(
+    [2,7,11,15],
+    9
+); // [0, 1]
+
+solve(
+    [3,2,4],
+    6
+); // [1, 2]
+
+solve(
+    [3, 3],
+    6
+); // [0, 1]
